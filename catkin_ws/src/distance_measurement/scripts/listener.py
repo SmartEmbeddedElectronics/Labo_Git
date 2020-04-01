ce2ee42af569ebc228ca450270a8321a53792b26
janble <61495643+janble@users.noreply.github.com>
2020-04-01 13:17:18 +0200

    Update listener.py

--- catkin_ws/src/distance_measurement/scripts/listener.py
+++ catkin_ws/src/distance_measurement/scripts/listener.py
@@ -4,21 +4,21 @@ import rospy
 from std_msgs.msg import String
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    distance = data.data
+    rospy.loginfo(rospy.get_caller_id() + ' I heard %s', distance)
+    if distance<5: #distance in cm
+        pub.publish('STOP')
+        rospy.loginfo(rospy.get_caller_id() + 'STOP signal has been sent')
+        
 
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
-
     rospy.Subscriber('/sonar_dist', String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
+    pub = rospy.Publisher('/comm_distance', String, queue_size=10)
+    
+    
     rospy.spin()
+    
 
 if __name__ == '__main__':
     listener()
