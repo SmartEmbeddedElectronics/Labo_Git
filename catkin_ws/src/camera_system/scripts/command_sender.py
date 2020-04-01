7f8f07d1dbcfe00f887ac331a0c185d9cfe63977
Laurens Wuyts <laurens.wuyts@gmail.com>
2020-04-01 11:49:41 +0000

    Add 'debouncer' for detection fix

--- catkin_ws/src/camera_system/scripts/command_sender.py
+++ catkin_ws/src/camera_system/scripts/command_sender.py
@@ -15,19 +15,21 @@ def callback(data):
     for m in data.markers:
 	if m.id == prev_id and now - prev_time < 10:
 	    print("Too early")
-	elif m.id == 1:
-	    comm_cam.publish("dance")
-	elif m.id == 2:
-	    comm_cam.publish("home")
-	elif m.id == 3:
-	    comm_cam.publish("play")
-	elif m.id == 4:
-	    comm_v_s.publish("start")
-	elif m.id > 100 and m.id < 200:
-	    distance = m.id - 100
-	    comm_cam.publish("move+" + str(distance))
 	else:
-	    comm_cam.publish("error")
+	    prev_time = now
+            if m.id == 1:
+	        comm_cam.publish("dance")
+            elif m.id == 2:
+                comm_cam.publish("home")
+            elif m.id == 3:
+	        comm_cam.publish("play")
+            elif m.id == 4:
+	        comm_v_s.publish("start")
+	    elif m.id > 100 and m.id < 200:
+	        distance = m.id - 100
+	        comm_cam.publish("move+" + str(distance))
+	    else:
+	        comm_cam.publish("error")
         print(m.id)
 
 def aruco_tag_interpret():
