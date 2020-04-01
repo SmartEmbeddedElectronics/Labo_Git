a4bc39bae70046f10a35a2eea0ccaac906cf2a40
janble <61495643+janble@users.noreply.github.com>
2020-04-01 13:25:44 +0200

    Update listener.py

--- catkin_ws/src/distance_measurement/scripts/listener.py
+++ catkin_ws/src/distance_measurement/scripts/listener.py
@@ -6,15 +6,15 @@ from std_msgs.msg import String
 def callback(data):
     distance = data.data
     rospy.loginfo(rospy.get_caller_id() + ' I heard %s', distance)
-    if distance<5: #distance in cm
-        rospy.loginfo(rospy.get_caller_id() + 'STOP signal has been sent')
-        pub.publish('STOP')
+    if int(distance)<5: #distance in cm
+        rospy.loginfo(rospy.get_caller_id() + 'stop signal has been sent')
+        pub.publish('stop')
         
 
 def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber('/sonar_dist', String, callback)
-    pub = rospy.Publisher('/comm_distance', String, queue_size=10)
+    pub = rospy.Publisher('comm_distance', String, queue_size=10)
     
     
     rospy.spin()
