71f1308cb33bdc2c7348cf10bcbaaaee2caec92f
Laurens Wuyts <laurens.wuyts@gmail.com>
2020-04-01 11:45:04 +0000

    Add 'debouncer' for detection

--- catkin_ws/src/camera_system/scripts/command_sender.py
+++ catkin_ws/src/camera_system/scripts/command_sender.py
@@ -2,25 +2,32 @@
 import rospy
 from std_msgs.msg import String
 from aruco_msgs.msg import MarkerArray, Marker
+from time import time
 
 comm_cam = rospy.Publisher('comm_cam', String, queue_size=10)
 comm_v_s = rospy.Publisher('comm_voice_start', String, queue_size=10)
 
+prev_time = time()
+prev_id = 0
+
 def callback(data):
+    now = time()
     for m in data.markers:
-	if m.id == 1:
-		comm_cam.publish("dance")
+	if m.id == prev_id and now - prev_time < 10:
+	    print("Too early")
+	elif m.id == 1:
+	    comm_cam.publish("dance")
 	elif m.id == 2:
-		comm_cam.publish("home")
+	    comm_cam.publish("home")
 	elif m.id == 3:
-		comm_cam.publish("play")
+	    comm_cam.publish("play")
 	elif m.id == 4:
-		comm_v_s.publish("start")
+	    comm_v_s.publish("start")
 	elif m.id > 100 and m.id < 200:
-		distance = m.id - 100
-		comm_cam.publish("move+" + str(distance))
+	    distance = m.id - 100
+	    comm_cam.publish("move+" + str(distance))
 	else:
-		comm_cam.publish("error")
+	    comm_cam.publish("error")
         print(m.id)
 
 def aruco_tag_interpret():
