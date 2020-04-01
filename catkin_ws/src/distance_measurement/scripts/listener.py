2ab41ed006da51abf1eb580963b06c4372d0ab12
janble <61495643+janble@users.noreply.github.com>
2020-04-01 11:32:42 +0200

    Update listener.py

--- catkin_ws/src/distance_measurement/scripts/listener.py
+++ catkin_ws/src/distance_measurement/scripts/listener.py
@@ -1,20 +1,24 @@
+#!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
 
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
 def listener():
-	pub = rospy.Publisher('/sonar_dist', String, queue_size=10)
-	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(1) # 10hz
-	while not rospy.is_shutdown():
-		#hello_str = "hello world %s" % rospy.get_time()
-		txt = raw_input("Enter your value: ") 
-		print(txt) 
-		rospy.loginfo(txt)
-		pub.publish(txt)
-	rate.sleep()
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('/sonar_dist', String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+    listener()
