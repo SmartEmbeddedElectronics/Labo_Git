8c210a96dc576236cf56cb76964080e80c489a32
Laurens Wuyts <laurens.wuyts@gmail.com>
2020-04-22 18:44:40 +0000

    Calculate degrees

--- catkin_ws/src/camera_system/scripts/detect_position.py
+++ catkin_ws/src/camera_system/scripts/detect_position.py
@@ -4,16 +4,22 @@ from std_msgs.msg import String
 from aruco_msgs.msg import MarkerArray, Marker
 from time import time
 import sys
+import numpy as np
 
 comm_pos = rospy.Publisher('comm_pos', String, queue_size=10)
 
 def callback(data):
     for m in data.markers:
 	if m.id == 5:
+            posx = m.pose.pose.position.x
+            posz = m.pose.pose.position.z
+
 	    print "\rTracking",
-            print m.pose.pose.position.x,
+            print posx,
             print m.pose.pose.position.y,
-            print m.pose.pose.position.z,
+            print posz,
+            deg  = (np.arctan2(posz, posx) * 180) / np.pi
+            print deg,
             sys.stdout.flush()
 
 def aruco_tag_tracking():
