ed3004f00f86d999acbe34a8f4005dd77246f682
Felix Lerner <lernerfelix@hotmail.com>
2020-04-29 13:31:59 +0200

    fixed beat per minute

--- catkin_ws/src/music/scripts/bpm_detection.py
+++ catkin_ws/src/music/scripts/bpm_detection.py
@@ -3,33 +3,13 @@
 from aubio import source, tempo
 from numpy import median, diff
 
-def get_file_bpm(path, params=None):
+def get_file_bpm(path):
     """ Calculate the beats per minute (bpm) of a given file.
         path: path to the file
         param: dictionary of parameters
     """
-    if params is None:
-        params = {}
     # default:
     samplerate, win_s, hop_s = 44100, 1024, 512
-    if 'mode' in params:
-        if params.mode in ['super-fast']:
-            # super fast
-            samplerate, win_s, hop_s = 4000, 128, 64
-        elif params.mode in ['fast']:
-            # fast
-            samplerate, win_s, hop_s = 8000, 512, 128
-        elif params.mode in ['default']:
-            pass
-        else:
-            raise ValueError("unknown mode {:s}".format(params.mode))
-    # manual settings
-    if 'samplerate' in params:
-        samplerate = params.samplerate
-    if 'win_s' in params:
-        win_s = params.win_s
-    if 'hop_s' in params:
-        hop_s = params.hop_s
 
     s = source(path, samplerate, hop_s)
     samplerate = s.samplerate
@@ -64,16 +44,4 @@ def get_file_bpm(path, params=None):
 
     return beats_to_bpm(beats, path)
 
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--mode',
-            help="mode [default|fast|super-fast]",
-            dest="mode", default='default')
-    parser.add_argument('sources',
-            nargs='+',
-            help="input_files")
-    args = parser.parse_args()
-    for f in args.sources:
-        bpm = get_file_bpm(f, params = args)
-        print("{:6s} {:s}".format("{:2f}".format(bpm), f))
+print(get_file_bpm("music.mp3"))
